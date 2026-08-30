51bafe489151f7625f63b7998e9166fec84956b3
SaudG5 <ssssgg9000.com>
2026-08-30 11:32:11 +0300

    0.4

--- shell.c
+++ shell.c
@@ -30,79 +30,11 @@ char *read_command(void)
 }
 
 /**
- * execute_command - Resolves PATH, ensures safety checks, and runs processes
- * @line: Command line input
- * @prog_name: Name of the executable (argv[0])
- * @line_count: Current command line number for error reporting
- */
-int execute_command(char *line, char *prog_name, unsigned int line_count)
-{
-	pid_t pid;
-	int status, i = 0;
-	char *args[1024];
-	char *token;
-	char *executable = NULL;
-
-	token = strtok(line, " \t\r\n");
-	while (token != NULL && i < 1023)
-	{
-		args[i++] = token;
-		token = strtok(NULL, " \t\r\n");
-	}
-	args[i] = NULL;
-
-	if (args[0] == NULL)
-		return (0);
-
-	if (strchr(args[0], '/') != NULL)
-	{
-		if (access(args[0], X_OK) == 0)
-			executable = strdup(args[0]);
-	}
-	else
-	{
-		executable = find_path(args[0]);
-	}
-
-	if (!executable)
-	{
-		fprintf(stderr, "%s: %u: %s: not found\n", prog_name, line_count, args[0]);
-		return (127);
-	}
-
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("Error");
-		free(executable);
-		return (1);
-	}
-
-	if (pid == 0)
-	{
-		if (execve(executable, args, environ) == -1)
-		{
-			perror(prog_name);
-			free(executable);
-			_exit(127);
-		}
-	}
-	else
-	{
-		wait(&status);
-		free(executable);
-		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
-	}
-	return (0);
-}
-
-/**
  * exit_builtin - Handles the exit built-in command and frees resources
  * @args: Array of tokenized arguments
  * @line: Allocated input line to be freed
  * @last_status: Exit status of the previous command
- * Return: 1 if exit was handled (terminates program), 0 otherwise
+ * Return: 1 if exit was handled, 0 otherwise
  */
 int exit_builtin(char **args, char *line, int last_status)
 {
@@ -112,4 +44,52 @@ int exit_builtin(char **args, char *line, int last_status)
 		exit(last_status);
 	}
 	return (0);
+}
+
+/**
+ * execute_command - Resolves PATH, ensures safety checks, and runs processes
+ * @line: Command line input
+ * @prog_name: Name of the executable (argv[0])
+ * @line_count: Current command line number for error reporting
+ * Return: Exit status of the command
+ */
+int execute_command(char *line, char *prog_name, unsigned int line_count)
+{
+	pid_t pid;
+	int status, i = 0;
+	char *args[1024], *token, *executable = NULL;
+	static int last_status;
+
+	token = strtok(line, " \t\r\n");
+	while (token && i < 1023)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " \t\r\n");
+	}
+	args[i] = NULL;
+
+	if (!args[0] || exit_builtin(args, line, last_status))
+		return (last_status);
+
+	if (strchr(args[0], '/'))
+		executable = (access(args[0], X_OK) == 0) ? strdup(args[0]) : NULL;
+	else
+		executable = find_path(args[0]);
+
+	if (!executable)
+	{
+		fprintf(stderr, "%s: %u: %s: not found\n", prog_name, line_count, args[0]);
+		return (last_status = 127);
+	}
+	pid = fork();
+	if (pid == -1)
+		return (perror("Error"), free(executable), last_status = 1);
+	if (pid == 0 && execve(executable, args, environ) == -1)
+		perror(prog_name), free(executable), _exit(127);
+
+	wait(&status);
+	free(executable);
+	if (WIFEXITED(status))
+		last_status = WEXITSTATUS(status);
+	return (last_status);
 }

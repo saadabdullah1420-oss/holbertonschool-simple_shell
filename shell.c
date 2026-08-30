68838b896d89fca1fb3b613d46be65051983c57f
SaudG5 <ssssgg9000.com>
2026-08-30 15:59:20 +0300

    betty

--- shell.c
+++ shell.c
@@ -25,7 +25,6 @@ char *read_command(void)
 		free(line);
 		return (NULL);
 	}
-
 	return (line);
 }
 
@@ -68,64 +67,24 @@ int exit_builtin(char **args, char *line, int last_status)
 }
 
 /**
- * execute_command - Resolves PATH, ensures safety checks, and runs processes
- * @line: Command line input
- * @prog_name: Name of the executable (argv[0])
- * @line_count: Current command line number for error reporting
- * Return: Exit status of the command
+ * run_execve - Helper to fork and execute a program
+ * @executable: Full path of the binary
+ * @args: Arguments array
+ * @prog_name: Name of the shell executable
+ * Return: Exit status
  */
-int execute_command(char *line, char *prog_name, unsigned int line_count)
+int run_execve(char *executable, char **args, char *prog_name)
 {
 	pid_t pid;
-	int status, i = 0;
-	char *args[1024];
-	char *token;
-	char *executable = NULL;
-	static int last_status = 0;
-
-	token = strtok(line, " \t\r\n");
-	while (token != NULL && i < 1023)
-	{
-		args[i++] = token;
-		token = strtok(NULL, " \t\r\n");
-	}
-	args[i] = NULL;
-
-	if (args[0] == NULL)
-		return (last_status);
-
-	if (env_builtin(args))
-		return (0);
-
-	if (exit_builtin(args, line, last_status))
-		return (last_status);
-
-	if (strchr(args[0], '/') != NULL)
-	{
-		if (access(args[0], X_OK) == 0)
-			executable = strdup(args[0]);
-	}
-	else
-	{
-		executable = find_path(args[0]);
-	}
-
-	if (!executable)
-	{
-		fprintf(stderr, "%s: %u: %s: not found\n", prog_name, line_count, args[0]);
-		last_status = 127;
-		return (last_status);
-	}
+	int status;
 
 	pid = fork();
 	if (pid == -1)
 	{
 		perror("Error");
 		free(executable);
-		last_status = 1;
-		return (last_status);
+		return (1);
 	}
-
 	if (pid == 0)
 	{
 		if (execve(executable, args, environ) == -1)
@@ -140,7 +99,50 @@ int execute_command(char *line, char *prog_name, unsigned int line_count)
 		wait(&status);
 		free(executable);
 		if (WIFEXITED(status))
-			last_status = WEXITSTATUS(status);
+			return (WEXITSTATUS(status));
 	}
+	return (0);
+}
+
+/**
+ * execute_command - Resolves PATH, ensures safety checks, and runs processes
+ * @line: Command line input
+ * @prog_name: Name of the executable (argv[0])
+ * @line_count: Current command line number for error reporting
+ * Return: Exit status of the command
+ */
+int execute_command(char *line, char *prog_name, unsigned int line_count)
+{
+	int i = 0;
+	char *args[1024], *token, *executable = NULL;
+	static int last_status;
+
+	token = strtok(line, " \t\r\n");
+	while (token != NULL && i < 1023)
+	{
+		args[i++] = token;
+		token = strtok(NULL, " \t\r\n");
+	}
+	args[i] = NULL;
+	if (args[0] == NULL)
+		return (last_status);
+	if (env_builtin(args))
+		return (0);
+	if (exit_builtin(args, line, last_status))
+		return (last_status);
+	if (strchr(args[0], '/') != NULL)
+	{
+		if (access(args[0], X_OK) == 0)
+			executable = strdup(args[0]);
+	}
+	else
+		executable = find_path(args[0]);
+	if (!executable)
+	{
+		fprintf(stderr, "%s: %u: %s: not found\n", prog_name, line_count, args[0]);
+		last_status = 127;
+		return (last_status);
+	}
+	last_status = run_execve(executable, args, prog_name);
 	return (last_status);
 }

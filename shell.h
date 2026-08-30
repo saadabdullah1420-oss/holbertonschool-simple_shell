2ba559f7c8ca54b03259ddc2e01f4f85a7d6f1c1
SaudG5 <ssssgg9000.com>
2026-08-30 16:01:19 +0300

    betty

--- shell.h
+++ shell.h
@@ -14,7 +14,8 @@ void display_prompt(void);
 char *read_command(void);
 int execute_command(char *line, char *prog_name, unsigned int line_count);
 int exit_builtin(char **args, char *line, int last_status);
-
+int handle_builtins(char **args, char *line, int last_status, int *handled);
+int run_execve(char *executable, char **args, char *prog_name);
 char *_getenv(const char *name);
 char *find_path(char *command);
 int env_builtin(char **args);
